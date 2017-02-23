f0876f709c75a2bc0797ba4f18ae53f63b2cc4d2
Matt Bartholomew <Bartholomew.m134@gmail.com>
2017-02-23 15:14:43 -0500

    Added Exit Game

---
+++ Minesweeper3D/Assets/Scripts/Menus/ExitGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour {
+
+	public void Exit() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+#else
+         Application.Quit();
+#endif
+    }
+}
