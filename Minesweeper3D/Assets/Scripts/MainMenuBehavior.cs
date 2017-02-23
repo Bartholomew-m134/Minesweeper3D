80a2052b93bbabb9519d24147fbe90da45fd3039
powerskyle26 <powers.411@osu.edu>
2017-02-23 15:04:04 -0500

    Added play buttons and about button

---
+++ Minesweeper3D/Assets/Scripts/MainMenuBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBehavior : MonoBehaviour {
+    public GameObject mainMenu;
+    public GameObject playMenu;
+    public GameObject aboutMenu;
+
+    public void GoToPlayMenu() {
+        DisableAll();
+        playMenu.SetActive(true);
+    }
+
+    public void GoToMainMenu() {
+        DisableAll();
+        mainMenu.SetActive(true);
+    }
+
+    public void GoToAboutMenu() {
+        DisableAll();
+        aboutMenu.SetActive(true);
+    }
+
+    private void DisableAll() {
+        mainMenu.SetActive(false);
+        playMenu.SetActive(false);
+        aboutMenu.SetActive(false);
+
+    }
+
+
+}
