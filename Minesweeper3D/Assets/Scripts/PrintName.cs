ff8f2675e563f4a5ce5e54726b92860ebbd81e76
Matt Bartholomew <Bartholomew.m134@gmail.com>
2017-02-23 11:07:22 -0500

    Implemented Basic Menu

---
+++ Minesweeper3D/Assets/Scripts/PrintName.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintName : MonoBehaviour {
+
+	public void Print() {
+        Debug.Log("Button Pressed: " + transform.name);
+    }
+}
