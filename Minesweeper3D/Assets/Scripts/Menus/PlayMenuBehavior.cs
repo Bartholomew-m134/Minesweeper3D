f1596529282a9cd7e5e44a4c84db7f4ce7aad678
powerskyle26 <powers.411@osu.edu>
2017-03-22 16:39:12 -0400

    Added scene loading functionality for play buttons

---
+++ Minesweeper3D/Assets/Scripts/Menus/PlayMenuBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMenuBehavior : MonoBehaviour {
+
+    public GameObject playMenu;
+
+    public void LaunchNew2DGame() {
+        SceneManager.LoadScene("Minesweeper");
+    }
+
+    public void LaunchNew3DGame() {
+        SceneManager.LoadScene("Minesweeper");
+    }
+}
