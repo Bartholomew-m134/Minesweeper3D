b74af1829c9a2254788d1afe8496b1d3883582a8
powerskyle26 <powers.411@osu.edu>
2017-03-24 11:26:38 -0400

    Added reset on shake

---
+++ Minesweeper3D/Assets/Scripts/AccelerometerBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AccelerometerBehavior : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.acceleration.magnitude > 2) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        Debug.Log(Input.acceleration.magnitude);
+    }
+}
