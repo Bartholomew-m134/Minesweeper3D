fa234bcd89f0e046536a382bc77fb06b5190c848
Matt Bartholomew <Bartholomew.m134@gmail.com>
2017-03-24 17:33:28 -0400

    Started Implementing 3D Bounds

--- Minesweeper3D/Assets/Scripts/Minesweeper/MinesweeperManager.cs
+++ Minesweeper3D/Assets/Scripts/Minesweeper/MinesweeperManager.cs
@@ -24,6 +24,7 @@ public class MinesweeperManager : MonoBehaviour {
         var capacity = gridLength * gridLength;
         if(Is3D) {
             capacity *= numberOfSides;
+            bombNumber *= bombNumber;
         }
 
         cellValues = new List<CellValue>(capacity);
@@ -31,7 +32,8 @@ public class MinesweeperManager : MonoBehaviour {
 
         var buttonList = GetComponentsInChildren<Button>();
         for (int i = 0; i < cellValues.Count; i++) {
-            buttonList[i].GetComponentInChildren<Text>().text = ((int)cellValues[i]).ToString();
+            //buttonList[i].GetComponentInChildren<Text>().text = ((int)cellValues[i]).ToString();
+            buttonList[i].GetComponentInChildren<Text>().text = (i).ToString();
         }
 	}
 	
@@ -93,20 +95,58 @@ public class MinesweeperManager : MonoBehaviour {
     private List<int> GetAdjacentIndexes(int centerIndex) {
         List<int> adjIndexes = new List<int>();
 
-        int beginningRow = (centerIndex < gridLength) ? 0 : -1;
-        int endingRow = (centerIndex >= (cellValues.Count - gridLength)) ? 1 : 2;
+        bool isLeftEdge = centerIndex < gridLength;
+        bool isRightEdge = centerIndex >= (cellValues.Count - gridLength);
+        bool isTopEdge = centerIndex % gridLength == 0;
+        bool isBottomEdge = ((centerIndex - 4) % gridLength == 0) && centerIndex != 0;
 
-        int beginningColumn = (centerIndex % gridLength == 0) ? 0 : -1;
-        int endingColumn = (((centerIndex - 4) % gridLength == 0) && centerIndex != 0) ? 1 : 2;
+        //if (!Is3D || !(isLeftEdge || isRightEdge || isTopEdge || isBottomEdge)) {
+            int beginningRow = isLeftEdge ? 0 : -1;
+            int endingRow = isRightEdge ? 1 : 2;
 
-        for (int row = beginningRow; row < endingRow; row++) {
-            for (int column = beginningColumn; column < endingColumn; column++) {
-                int adjIndex = centerIndex + (row * gridLength) + column;
+            int beginningColumn = isTopEdge ? 0 : -1;
+            int endingColumn = isBottomEdge ? 1 : 2;
 
-                if(IsWithinBounds(adjIndex)) {
-                    adjIndexes.Add(adjIndex);
+            for (int row = beginningRow; row < endingRow; row++) {
+                for (int column = beginningColumn; column < endingColumn; column++) {
+                    int adjIndex = centerIndex + (row * gridLength) + column;
+
+                    if (IsWithinBounds(adjIndex)) {
+                        adjIndexes.Add(adjIndex);
+                    }
                 }
             }
+        //} 
+        
+        if (Is3D) {
+            switch (centerIndex) {
+                case 0:
+                    adjIndexes.Add(79);
+                    adjIndexes.Add(84);
+                    adjIndexes.Add(120);
+                    adjIndexes.Add(121);
+                    break;
+            }
+        }
+
+        /*for (int row = -1; row < 2; row++) {
+            for (int column = -1; column < 2; column++) {
+                //int columnAdjustment = column + (isRightEdge && column == 1 ? gridLength * gridLength - gridLength + 1: 0) - (isLeftEdge && column == -1 ? gridLength * gridLength - gridLength + 1 : 0);
+                int columnAdjustment = column + (isRightEdge && column == 1 ? gridLength * gridLength - gridLength + 1 : 0);
+                int adjIndex = centerIndex + (row * gridLength) + columnAdjustment;
+
+                if (IsWithinBounds(adjIndex)) {
+                    adjIndexes.Add(adjIndex);
+                }
+
+                Debug.Log((adjIndex + cellValues.Count) % cellValues.Count);
+                adjIndexes.Add((adjIndex + cellValues.Count) % cellValues.Count);
+            }
+        }*/
+
+        if(centerIndex == 0) {
+            foreach(var item in adjIndexes)
+            Debug.Log(item);
         }
 
         return adjIndexes;
