05f699cbc34d4fe39030f829aa5c1b8ebe17142b
powerskyle26 <powers.411@osu.edu>
2017-03-24 11:55:52 -0400

    Added buttons to rotate 3D grid

---
+++ Minesweeper3D/Assets/Scripts/GameButtonBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameButtonBehavior : MonoBehaviour {
+
+    public GameObject grid;
+    // Use this for initialization
+    public void UpButtonPress() {
+        grid.transform.Rotate(45, 0, 0);
+    }
+
+    public void DownButtonPress()
+    {
+        grid.transform.Rotate(-45, 0, 0);
+    }
+
+    public void RightButtonPress() {
+        grid.transform.Rotate(0, 45, 0);
+    }
+
+    public void LeftButtonPress() {
+        grid.transform.Rotate(0, -45, 0);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
