ec9b6d52b96a6a6ad94b9dc531daa43e2ab48385
powerskyle26 <powers.411@osu.edu>
2017-03-24 12:15:39 -0400

    Added timer to 2D and 3D minesweeper

---
+++ Minesweeper3D/Assets/Scripts/TimerUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUpdate : MonoBehaviour {
+    public Text timerText;
+    private float time;
+	// Use this for initialization
+	void Start () {
+        time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        timerText.text = time.ToString("F2");
+	}
+}
